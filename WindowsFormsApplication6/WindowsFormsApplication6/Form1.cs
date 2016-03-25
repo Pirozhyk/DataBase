fa4b57e952bd560190b515bf479208411ac01461
Pirozhyk <loveacmilan27@gmail.com>
2016-03-25 18:39:14 +0200

    Added files via upload

---
+++ WindowsFormsApplication6/WindowsFormsApplication6/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form patients = new patients();
+            Form Form1 = new Form1();
+            Form1.Close();
+            patients.Show();
+        }
+    }
+}
---
+++ WindowsFormsApplication6/WindowsFormsApplication6/patients.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication6
+{
+    public partial class patients : Form
+    {
+        public patients()
+        {
+            InitializeComponent();
+        }
+
+        private void patientBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.patientBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.hospitalDataSet);
+
+        }
+
+        private void patients_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "hospitalDataSet.Patient". При необходимости она может быть перемещена или удалена.
+            this.patientTableAdapter.Fill(this.hospitalDataSet.Patient);
+
+        }
+    }
+}
